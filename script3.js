ffedbcf5274c1c7d900ed6c1add1266332ea8ecc
Muhammad Waqar Ahmed <73448267+waqar172@users.noreply.github.com>
2021-03-29 01:22:20 +0100

    The Temperature Converter
    The Temperature Converter Task

---
+++ script3.js
@@ -0,0 +1,7 @@
+var inputCelsius  = 25;
+var outputFahrenheit = (inputCelsius * 9)/5 + 32;
+console.log(inputCelsius + '\u00B0 Celsious is ' + outputFahrenheit + '\u00B0 Fahrenheit');
+
+var inputFahrenheit  = 73;
+var outputCelsius = (inputFahrenheit - 32)/9 * 5;
+console.log(inputFahrenheit + '\u00B0 Fahrenheit is ' + outputCelsius + '\u00B0 Celsious.');
