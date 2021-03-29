4aa870ef2bcd3d059b0c13637c08aea2f87e342a
Muhammad Waqar Ahmed <73448267+waqar172@users.noreply.github.com>
2021-03-29 01:19:58 +0100

    Using Math Function
    Using Math Function Task

---
+++ script2.js
@@ -0,0 +1,3 @@
+var inputFahrenheit  = Math.random()*100;
+var outputCelsius = (inputFahrenheit - 32)/9 * 5;
+console.log('It is ' + inputFahrenheit + '\u00B0 Fahrenheit today. That\'s ' + outputCelsius + '\u00B0 Celsius.');
